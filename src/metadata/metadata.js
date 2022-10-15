d592d109fbe2b2a348de12727b80db942db4a65c
fffan64 <ff.fan@hotmail.fr>
2022-10-16 00:46:43 +0900

    1 modif

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,53 +1,84 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: 'Jonathan Vacherat',
+  title: 'Lead Sofware Engineer',
   facts: {
-    'Residence': '<a href="https://www.google.com/maps/place/London,+UK/@51.5285582,-0.2416814,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862"><i class="fa fa-home fact-icon"></i>London</a>',
-    'Wiki': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <i class="fa-brands fa-wikipedia-w fact-icon"></i>William_Shakespeare
+    'Residence': '<a href="https://goo.gl/maps/A8iRxqwVuiePLa1W6"><i class="fa fa-home fact-icon"></i>Tokyo</a>',
+    'LinkedIn': `<a href="https://www.linkedin.com/in/jonathanvacherat">
+                  <i class="fab fa-linkedin fact-icon"></i>Jonathan Vacherat
                 </a>`,
-    'Twitter': `<a href="https://twitter.com/shakespeare"><i class="fa-brands fa-twitter fact-icon"></i>Shakespeare</a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com"><i class="fa fa-envelope fact-icon"></i>wil64@yahoo.com</a>'
+    'GitHub': `<a href="https://github.com/fffan64"><i class="fab fa-github fact-icon"></i>fffan64</a>`,
+    'Email': '<a href="mailto:jonathan.vacherat@gmail.com"><i class="fa fa-envelope fact-icon"></i>jonathan.vacherat@gmail.com</a>'
   },
+  summary: `I'm a full-stack developer with more than 10 years of experience.
+  Love serverless & automation, gaming also !
+  Married and proud father of two <3`, 
   skills: [
-    ['Comedy', 100],
-    ['Love', 100],
-    ['Theater', 100],
-    ['Tragedy', 100],
-    ['Poetry', 100],
-    ['Erotic', 75],
-    ['Sonnets', 100],
-    ['Plays', 100],
-    ['Writing', 100],
-    ['Directing', 75],
-    ['Literature', 100],
-    ['Influence', 100],
+    ['Technical', 100],
+    ['Management', 50],
+    ['Team spririt', 100],
+    ['Problem solver', 100],
   ],
   positions: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
-      contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
+      title: 'AXA Life Japan, Tokyo, JP',
+      period: 'Jun 2017 - ?',
+      skills: ['Cloud', 'AWS', 'Openshift', 'Jenkins', 'Serverless', 'NodeJS', 'React', 'Jest', 'TypeScript', 'Java / Springboot'],
+      contents: `Lead Software Developer`
     },
     {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
+      title: 'Orano Japan (ex AREVA), Tokyo, JP',
+      period: 'Jun 2015 - Jun 2017',
+      skills: ['System', 'Network', 'Support', 'Tool/Scripts', 'Reporting'],
+      contents: `IT Engineer`
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
+      title: 'LAFON SAS (MADIC Industries), Bordeaux, FR',
+      period: 'Sep 2011 - May 2015',
+      skills: ['C#', 'Centura/SQL Windows', 'SQL Server', 'C/C++'],
+      contents: `R&D Software Engineer`
+    },
+    {
+      title: 'AKKA Technologies I&S, Bordeaux, FR',
+      period: 'Apr 2011 - Jul 2011',
+      skills: ['3D Lenses Screen', 'Shaders / GLSL', 'OpenGL', 'C/C++'],
+      contents: `R&D Software Engineer`
+    },
+    {
+      title: 'Techniques Effects, Pau, FR',
+      period: 'Jan 2011 - Apr 2011',
+      skills: ['Gaming', '3D', 'Shiva3D engine', 'Scripting LUA'],
+      contents: `Software Developer - 2D/3D Production Assist`
     }
+  ],
+  education: [
+    {
+      title: '<b>Computer Engineering Degree</b> - <a href="https://www.imerir.com/">IMERIR</a>, France [2007 - 2010]',
+      inParallel: [
+        'ERASMUS (Student exchange) Bachelor of Science in Computing - Institute of Technology Blanchardstown, Ireland [2009 - 2010]',
+        'Master in Sciences, Technologies, Health - Universite de Perpignan, France [2008 - 2009]'
+      ]
+    },
+    {
+      title: 'Undergraduate Technology Degree in Electrical Engineering and Industrial Computing - Universite de Toulouse, France [2005 - 2007]'
+    }
+  ],
+  certifications: [
+    '<a href="https://www.credly.com/badges/279c48c5-674f-4293-8f15-9325c3d47a60/public_url"><i class="fab fa-aws"></i>  AWS Certified Developer Associate</a> - Jan 24, 2020',
+    '<a href="https://www.credential.net/1ccbec45-3612-4cef-84b6-405b2b194a5c"><i class="fab fa-google"></i>  Google Cloud Certified Engineer Associate</a> - Aug 5, 2021'
+  ],
+  languages: [
+    ['French', 100],
+    ['English', 100],
+    ['Spanish', 75],
+    ['Japanese', 50],
+  ],
+  extra: [
+    'Gaming',
+    'Football',
+    'Art/3D',
+    'Movie',
+    'TV Shows/Anime',
+    'Technology'
   ],
   experience: [
     {
